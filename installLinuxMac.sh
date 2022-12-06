42071bfe9cfa2d25ac69d583390ccbd0d5dd7441
Daniel Costa <git@danielcosta.pt>
2022-12-06 11:31:00 +0000

    add alias

--- installLinuxMac.sh
+++ installLinuxMac.sh
@@ -193,6 +193,7 @@ installReq(){
     ## Test
     command -v git >/dev/null 2>&1 || { echo >&2 "git is required but it's not installed. Aborting."; exit 1; }
     command -v curl >/dev/null 2>&1 || { echo >&2 "curl is required but it's not installed. Aborting."; exit 1; }
+    alias python="python3"
     command -v python >/dev/null 2>&1 || { echo >&2 "python is required but it's not installed. Aborting."; exit 1; }
     mkdir -p ~/go/bin
 
